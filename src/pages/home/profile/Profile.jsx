7b9bc71803f15942dd90fd6b63abd075135aa25d
John Bailey <jbailey679@yahoo.com>
2024-03-14 10:51:49 -0500

    first commit

---
+++ src/pages/home/profile/Profile.jsx
@@ -0,0 +1,46 @@
+import "./profile.css";
+// import Rightbar from "../../components/rightbar/Rightbar";
+// import Sidebar from "../../components/sidebar/Sidebar";
+// import Topbar from "../../components/topbar/Topbar";
+// import Feed from "../../components/feed/Feed";
+import Rightbar from "../../../components/rightbar/Rightbar";
+import Sidebar from "../../../components/sidebar/Sidebar";
+import Feed from "../../../components/feed/Feed";
+import Topbar from "../../../components/topbar/Topbar";
+
+function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoNam">Marcus Carter</h4>
+              <span className="profileInfoDesc">Hello, my friends</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
